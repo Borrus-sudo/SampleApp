313fcc435a561725bbff7e87f805fef4ebe828b1
(TS/JS).Gandalf(he/him) <58482194+Borrus-sudo@users.noreply.github.com>
2021-08-26 17:23:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 function searchEasterEggs() {
     //make easter eggs
-    console.log("%c Easter egg!!!!!", "color:red;");
-    alert("Find the easter eggs!!!!!!");
     window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 }
 searchEasterEggs();
